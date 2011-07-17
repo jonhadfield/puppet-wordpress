88bb90f55587e4fcdfa9c2056d42c9e5dbaee693
Jon <jon@lessknown.co.uk>
2011-07-17 17:43:09 +0100

    init.pp
    Added working smoke test.
    Run with: puppet apply init.pp --verbose --modulepath=<path to modules> --noop

--- tests/init.pp
+++ tests/init.pp
@@ -1,1 +1,5 @@
-include wordpress
+class {'wordpress':
+                wordpress_db_name =>      "mydbname",
+                wordpress_db_user =>      "mydbuser",
+                wordpress_db_password =>  "mydbpassword"
+}
